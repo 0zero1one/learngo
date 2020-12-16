92c701f3f82b6b5c0f66ca09616bded11065f954
fengzhiyong <2117538193@qq.com>
2020-12-16 16:05:32 +0800

    爬虫程序进行不下去了，先插眼，以后在搞

--- crawier/main.go
+++ crawier/main.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"net/http"
+	"learngo/crawier/engine"
+	"learngo/crawier/zhenai/parser"
 )
-import "fmt"
 
 func main() {
-	//engine.Run(engine.Request{
-	//	Url:        "http://www.zhenai.com/zhenghun",
-	//	ParserFunc: parser.ParseCityList,
-	//})
-
-	resp, err := http.Get("https://album.zhenai.com/api/profile/getObjectProfile.do?objectID=1976352655&_=16" +
-		"08100599437&ua=h5%2F1.0.0%2F1%2F0%2F0%2F0%2F0%2F0%2F" +
-		"%2F0%2F0%2F4450d139-9bea-4f8b-8c53-16d06c0783f3%2F0%2F0%2F1578333505")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	fmt.Printf("%v", resp.Body)
+	engine.Run(engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
